5f74da12ad772512d64e1c577343ce14fd4d8390
andy5995 <andy400-dev@yahoo.com>
2019-08-19 19:45:38 -0500

    sock.c:remove unneeded includes

--- src/sock.h
+++ src/sock.h
@@ -1,3 +1,30 @@
+/*
+ * sock.h
+ *
+ * Copyright 2018-2019 Andy Alt <andy400-dev@yahoo.com>
+ *
+ * modemu2k is a fork of modemu
+ * Originally developed by Toru Egashira
+ * Copyright (c) 1995, 1996
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ *
+ *
+ */
+
 struct
 {
   int fd;
